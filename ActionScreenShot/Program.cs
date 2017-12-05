d9ebb0374ea2734b187dfb007eb6a1670d43bbfe
Jose Alejandro Mendez <jam@datacenter.cm>
2017-12-05 14:22:42 -0400

    Funcion CapturarImagen generada

--- ActionScreenShot/Program.cs
+++ ActionScreenShot/Program.cs
@@ -8,52 +8,42 @@ namespace ActionScreenShot
 {
     class Program
     {
+        public static string date = DateTime.Now.ToString().Replace("/", "-").Replace(":", "_").Replace(" ", "_"); // Obtener la fecha
+        public static string path = AppDomain.CurrentDomain.BaseDirectory; // Definir la ruta
+        public static string fileName = path + "\\Screenshots\\screenshot" + date + ".jpg";
+
+        public static void CapturarImagen()
+        {
+            // Hacer captura de pantalla
+            Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
+
+            Graphics graphics = Graphics.FromImage(printscreen as Image);
+            graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
+
+            //Guardar captura de pantalla
+            printscreen.Save(fileName, ImageFormat.Jpeg);
+        }
+
+
         static void Main(string[] args)
         {
             try
-            {
-                string date = DateTime.Now.ToString().Replace("/", "-").Replace(":", "_").Replace(" ", "_"); // Obtener la fecha                
-                string path = AppDomain.CurrentDomain.BaseDirectory; // Definir la ruta
-                string fileName = path + "\\Screenshots\\screenshot" + date + ".jpg";
-                                
+            {                   
                 if (!Directory.Exists(path + "\\Screenshots"))
                 {
                     // Crear repositorio si no existe
                     Directory.CreateDirectory(path + "\\Screenshots");
-                    
-                    // Hacer captura de pantalla
-                    Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
-                    
-                    Graphics graphics = Graphics.FromImage(printscreen as Image);
-                    graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
-
-                    //Guardar captura de pantalla
-                    printscreen.Save(fileName, ImageFormat.Jpeg);
+                    CapturarImagen();                    
                 }
                 else
                 {
-                    // Hacer captura de pantalla
-                    Bitmap printscreen = new Bitmap(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height, PixelFormat.Format32bppArgb);
-                    
-                    Graphics graphics = Graphics.FromImage(printscreen as Image);
-                    graphics.CopyFromScreen(0, 0, 0, 0, printscreen.Size);
-
-                    //Guardar captura de pantalla
-                    printscreen.Save(fileName, ImageFormat.Jpeg);
+                    CapturarImagen();
                 }
-
-
-                
-                
-
-                
-                //Console.WriteLine(date);
-                //Console.ReadKey();
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error encontrado ", e);
             }
-        }
+        }        
     }
 }
