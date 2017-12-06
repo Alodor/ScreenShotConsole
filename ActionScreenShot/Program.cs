7fe4aa31efcc800cf8b85d3b9f4e011148307a2c
Jose Alejandro Mendez <jam@datacenter.cm>
2017-12-06 15:28:02 -0400

    Nueva ruta para guardar las capturas

--- ActionScreenShot/Program.cs
+++ ActionScreenShot/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Windows.Forms;
@@ -9,9 +10,10 @@ namespace ActionScreenShot
     class Program
     {
         public static string date = DateTime.Now.ToString().Replace("/", "-").Replace(":", "_").Replace(" ", "_"); // Obtener la fecha
+        public static string dateFolder = DateTime.Now.ToShortDateString().Replace("/", "-");
         public static string path = AppDomain.CurrentDomain.BaseDirectory; // Definir la ruta del proyecto
-        public static string workDirectory = path + "\\Screenshots";
-        public static string fileName = path + "\\Screenshots\\screenshot" + date + ".jpg";
+        public static string workDirectory = path + "\\Screenshots\\" + dateFolder; // Directorio para repositorio de imagenes
+        public static string fileName = path + "\\Screenshots\\" + dateFolder + "\\screenshot " + date + ".jpg"; // Guardar imagen en el directorio especifico
 
         public static void CapturarImagen()
         {
@@ -28,6 +30,31 @@ namespace ActionScreenShot
         }
 
 
+        public static void RedimensionarImagen()
+        {
+            try
+            {
+                // Ubicar directorio de trabajo
+                DirectoryInfo directory = new DirectoryInfo(workDirectory);       
+                
+                // Ordenar imagenes de forma descendente
+                FileInfo[] allFiles = directory.GetFiles().OrderByDescending(f => f.CreationTime).ToArray();               
+
+                // 
+                foreach (var f in allFiles)
+                {
+                    string x = f.Name;
+                    Console.WriteLine(x);
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error encontrado: {0}", e.ToString());
+            }            
+        }
+
+
         static void Main(string[] args)
         {
             try
@@ -36,16 +63,18 @@ namespace ActionScreenShot
                 {
                     // Crear repositorio si no existe
                     Directory.CreateDirectory(workDirectory);
-                    CapturarImagen();                    
+                    CapturarImagen();
+                    RedimensionarImagen();
                 }
                 else
                 {
                     CapturarImagen();
+                    RedimensionarImagen();
                 }
             }
             catch (Exception e)
             {
-                Console.WriteLine("Error encontrado ", e);
+                Console.WriteLine("Error encontrado: {0}", e.ToString());
             }
         }        
     }
