d34bd457f230a81772fd0ca1b5608867c99713b7
Jose Alejandro Mendez <jam@datacenter.cm>
2017-12-07 15:58:11 -0400

    Funcion RenderizarImagen integrada

--- ActionScreenShot/Program.cs
+++ ActionScreenShot/Program.cs
@@ -14,8 +14,8 @@ namespace ActionScreenShot
         public static string path = AppDomain.CurrentDomain.BaseDirectory; // Definir la ruta del proyecto
         public static string workDirectory = path + "\\Screenshots\\" + dateFolder; // Directorio para repositorio de imagenes
         public static string fileName = path + "\\Screenshots\\" + dateFolder + "\\screenshot_" + date + ".jpg"; // Guardar imagen en el directorio especifico
-        public static string imagenReciente = "";
-
+        public static string temp = path + "\\Temp"; // Directorio temporal
+        public static string tempImage = temp + "\\screenshot_" + date + ".jpg"; // Guardar imagen en el directorio temporal
 
         public static void CapturarImagen()
         {
@@ -39,7 +39,7 @@ namespace ActionScreenShot
         }
 
 
-        public static void ObtenerImagenReciente()
+        public static void RenderizarImagen()
         {
             try
             {
@@ -47,25 +47,70 @@ namespace ActionScreenShot
                 DirectoryInfo directory = new DirectoryInfo(workDirectory);
 
                 // Ordenar imagenes de forma descendente
-                FileInfo[] allFiles = directory.GetFiles().OrderByDescending(f => f.CreationTime).ToArray();                
+                FileInfo[] allFiles = directory.GetFiles().OrderByDescending(f => f.CreationTime).ToArray();
 
+                string imagenReciente = "";
+
+                // Seleccionar la primera imagen
                 foreach (var f in allFiles)
                 {
                     imagenReciente = f.Name;
                     break;
                 }
 
+                // Seleccionar imagen reciente
+                using (var image = Image.FromFile(workDirectory + "\\" + imagenReciente))
+
+                // Usar funcion para escalar la imagen seleccionada
+                using (var newImage = EscalarImagen(image, 600, 600))
+                {
+                    // Crear directorio temporal
+                    if (!Directory.Exists(temp))
+                        Directory.CreateDirectory(temp);
+
+                    // Guardar la imagen en el directorio temporal
+                    newImage.Save(tempImage, ImageFormat.Jpeg);
+
+                    
+
+                    /*
+                    if (File.Exists(workDirectory + "\\" + imagenReciente))
+                    {
+                        string imagenDelete = workDirectory + "\\" + imagenReciente;
+                        //File.Delete(imagenDelete);
+                        //Console.WriteLine("el archivo a eliminar es: " + imagenDelete);
+                    }
+                    */
+
+                    //newImage.Save(@"c:\test.jpg", ImageFormat.Jpeg);
+                }
+
+                // Eliminar la imagen original
+                string imagenDelete = workDirectory + "\\" + imagenReciente;
+                File.Delete(imagenDelete);
+
+                // Funcion para escalado de imagen
+                Image EscalarImagen(Image image, int maxWidth, int maxHeight)
+                {
+                    var ratioX = (double)maxWidth / image.Width;
+                    var ratioY = (double)maxHeight / image.Height;
+                    var ratio = Math.Min(ratioX, ratioY);
+
+                    var newWidth = (int)(image.Width * ratio);
+                    var newHeight = (int)(image.Height * ratio);
+
+                    var newImage = new Bitmap(newWidth, newHeight);
+
+                    using (var graphics = Graphics.FromImage(newImage))
+                    graphics.DrawImage(image, 0, 0, newWidth, newHeight);
+
+                    return newImage;
+                }
             }
             catch (Exception e)
             {
                 Console.WriteLine("Error encontrado: {0}", e.ToString());
-            }            
-        }
-
-
-        public static void EscalarImagen()
-        {
-
+            }
         }
 
 
@@ -75,17 +120,10 @@ namespace ActionScreenShot
             {          
                 // Comprobar existencia del repositorio
                 if (!Directory.Exists(workDirectory))
-                {
-                    // Crear repositorio si no existe
                     Directory.CreateDirectory(workDirectory);
-                    CapturarImagen();
-                    ObtenerImagenReciente();
-                }
-                else
-                {
-                    CapturarImagen();
-                    ObtenerImagenReciente();
-                }
+
+                CapturarImagen();
+                RenderizarImagen();
             }
             catch (Exception e)
             {
